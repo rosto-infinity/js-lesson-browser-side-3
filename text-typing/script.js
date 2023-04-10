f2327a12d93923cb51f5245f929956ae0ed9e6ae
rosto-infinity <rosto.infocus@gmail.com>
2023-04-09 22:18:14 -1100

    writeTyping

--- text-typing/script.js
+++ text-typing/script.js
@@ -4,8 +4,43 @@ let array = [
     "Graphiste",
     " Créatif"
 ];
-target.textContent += array[0];
-target.style.color += "red";
+let wordIndex = 0;
+let letterIndex = 0;
+//  
 // target.textContent += array[1];
 // target.textContent += array[2];
 // target.style.color = "yellow";
+
+const createLetter = ()=> {
+    const letter = document.createElement("span");
+    target.appendChild(letter);
+   
+    letter.textContent = array[wordIndex][letterIndex];
+    setTimeout(() => {
+        letter.remove();
+        
+    }, 2800);
+}
+//createLetter();
+// setInterval(createLetter, 200);
+const loop = () =>{
+    setTimeout(() => {
+        if(wordIndex >=array.length){
+            wordIndex = 0;
+            letterIndex = 0;
+            loop();
+        }else if(letterIndex < array[wordIndex].length){
+            createLetter();
+            letterIndex++;
+            loop();
+        }else{
+            wordIndex++;
+            letterIndex = 0;
+            setTimeout(() => {
+               
+                loop();
+            }, 2800);
+        }    
+    }, 60);
+}
+loop();

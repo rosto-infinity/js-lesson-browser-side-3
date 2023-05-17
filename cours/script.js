5d4730150afed4545a5322f77a5d2a106a71deeb
rosto-infinity <rosto.infocus@gmail.com>
2023-05-17 03:17:30 -1100

    dataset

--- cours/script.js
+++ cours/script.js
@@ -63,6 +63,9 @@ let data = [
 ];
 // console.log(data[2].technos);
 
+
+
+
 //---------------------------
 // Les Structures de controle
 //---------------------------
@@ -103,3 +106,67 @@ document.body.addEventListener(
     });
 
   
+//-----------------------------------
+// Les datasets
+//-----------------------------------
+
+// En JavaScript, les datasets font référence aux attributs de données personnalisées que les développeurs peuvent définir sur les éléments HTML. Ces attributs fournissent un moyen de stocker des informations supplémentaires sur un élément HTML. Les datasets permettent aux développeurs de stocker et de récupérer des données personnalisées associées à un élément HTML spécifique, ce qui peut être très utile dans la manipulation des éléments HTML dans le code JavaScript.
+
+// Par exemple, si vous avez un formulaire HTML pour créer un compte utilisateur, vous pouvez ajouter des datasets personnalisées pour stocker le nom d'utilisateur, l'adresse e-mail, etc.
+
+// Accéder aux datasets personnalisées
+const monElement = document.querySelector('#monElement');
+
+const nameN = monElement.dataset.name;
+const age = monElement.dataset.age;
+
+console.log(nameN); // "John"
+console.log(age); // "30"
+
+// Définir des datasets personnalisées
+monElement.dataset.hobbies = 'Soccer, Reading';
+console.log(monElement.dataset.hobbies); // "Soccer, Reading"
+
+// Dans cet exemple, nous avons défini deux datasets personnalisées data-name et data-age sur un élément div. Nous avons également accédé aux valeurs de ces datasets à l'aide de la propriété dataset de l'élément HTML. Ensuite, nous avons ajouté un nouveau dataset personnalisé data-hobbies à l'élément et nous avons accédé à sa valeur pour vérifier si elle a été correctement ajoutée.
+
+
+
+const h1js = document.getElementById("javascript");
+//console.log(h1js);
+//console.log(h1js.dataset);
+const h1 = document.querySelectorAll("h1");
+h1.forEach(
+    (language) => console.log(language.dataset.lang)
+);
+
+//----------------------------
+//Les Regex
+//----------------------------
+
+// /^ : commence la chaîne de caractères à vérifier.
+
+// [a-z0-9._-] : autorise des caractères alphanumériques (a-z et 0-9), le point, le tiret bas et le trait d'union. Les crochets signifient que n'importe lequel de ces caractères peut être utilisé.
+
+// + : autorise une ou plusieurs fois le caractère précédent. Cela signifie que l'adresse e-mail doit contenir au moins un des caractères spécifiés ci-dessus.
+
+// @ : la chaîne doit contenir un caractère @.
+
+// [a-z0-9._-]{2,} : de nouveaux caractères autorisés après le @, avec au moins 2 occurrences (ce qui signifie que le domaine doit contenir au moins deux caractères).
+
+// \. : un point (.) doit être échappé avec un antislash ().
+
+// [a-z]{2,4}$ : le domaine peut contenir de 2 à 4 caractères alphabétiques. Le $ signifie que la chaîne doit se terminer ici.
+
+// .test(email); : La méthode test () est appelée sur ce regex pour vérifier si l'adresse email satisfait à cette expression régulière. Si l'adresse email correspond à cette expression régulière, la méthode retournera true, sinon elle retournera false. Notez que la variable email doit être remplacée par l'adresse email que vous voulez vérifier
+
+let gge =/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,12}$/.test(password);
+
+// /^ : commence la chaîne de caractères à vérifier.
+
+// (?=.*[0-9]) : utilise une assertion positive pour vérifier si la chaîne contient au moins un chiffre (0-9).
+
+// (?=.*[!@#$%^&*]) : utilise une autre assertion positive pour vérifier si la chaîne contient au moins un caractère spécial (!, @, #, $, %, ^, &, *).
+
+// [a-zA-Z0-9!@#$%^&*]{8,12}$ : spécifie que la chaîne doit être composée de caractères alphanumériques (a-z, A-Z et 0-9) ainsi que des caractères spéciaux mentionnés ci-dessus, et doit avoir une longueur comprise entre 8 et 12 caractères.
+
+// .test(password) : La méthode test () est appelée sur ce regex pour vérifier si le mot de passe satisfait à cette expression régulière. Si le mot de passe correspond à cette expression régulière, la méthode retournera true, sinon elle retournera false. Notez que la variable password doit être remplacée par le mot de passe que vous voulez vérifier.
--- passeword/script.js
+++ passeword/script.js
@@ -6,25 +6,49 @@ const dataNumbers ="0123456789";
 const dataSymbols = "()&#{[|`\^@]}%¨ !\"?;,*$!"
 const rangeValue = document.getElementById("password-length");
 // console.log(rangeValue);
+const passwordOutput = document.getElementById("password-output");
 
 function generateurPassword(){
     let data = [];
     let password = "";
 
-    if(lowercase.checked)data.push(dataLowercase);
-    if(uppercase.checked)data.push(dataUppercase);
-    if(numbers.checked)data.push(dataNumbers);
-    if(symbols.checked)data.push(dataSymbols);
+    if(lowercase.checked)data.push(...dataLowercase);
+    if(uppercase.checked)data.push(...dataUppercase);
+    if(numbers.checked)data.push(...dataNumbers);
+    if(symbols.checked)data.push(...dataSymbols);
+
+    // Ce script JavaScript permet de générer une chaîne de caractères aléatoire en fonction des options sélectionnées par l'utilisateur.
+
+
+    // Tout d'abord, il vérifie si la case « lowercase » est cochée. Si elle l'est, il ajoute les caractères en minuscules, stockés dans un tableau appelé dataLowercase, dans un tableau appelé data. La syntaxe data.push(...dataLowercase); permet d'ajouter tous les éléments d'un tableau dans un autre.
+    
+    
+    // Ensuite, le script vérifie si la case « uppercase » est cochée. Si elle l'est, il ajoute les caractères en majuscules, stockés dans un tableau appelé dataUppercase, dans le tableau data.
+    
+    
+    // Puis, il vérifie si la case « numbers » est cochée. Si elle l'est, il ajoute les chiffres, stockés dans un tableau appelé dataNumbers, dans le tableau data.
+    
+    
+    // Enfin, si la case « symbols » est cochée, il ajoute les symboles, stockés dans un tableau appelé dataSymbols, dans le tableau data.
+    
+    
+    // À la fin de ce script, le tableau data contient tous les caractères qui ont été sélectionnés par l'utilisateur pour générer une chaîne de caractères aléatoire.
+
 
     if(data.length === 0){
         alert("Veuillez selectionner des critères ");
         return;
+        // si la variable « data » contient des éléments. Si elle est vide, l'utilisateur n'a pas sélectionné d'options et une alerte est affichée pour demander à l'utilisateur de sélectionner des critères. Le script s'arrête grâce à l'opérateur « return ».
     }
     for(i = 0;  i < rangeValue.value; i++){
-        password = data[Math.floor(Math.random()) * data.length];
-        console.log(password);
-        console.log(data);
-    }   
+        password += data[Math.floor(Math.random() * data.length)];
+        // console.log(password);
+        
+    }  
+    // une boucle « for » pour créer un mot de passe aléatoire avec un nombre de caractères défini par la variable « rangeValue.value ». Cette variable est la valeur sélectionnée par l'utilisateur à l'aide d'une balise « input » ou « range ».
+
+    // À chaque tour de boucle, le script choisit au hasard un caractère dans la liste « data » en utilisant la fonction Math.floor(Math.random()) * data.length. Cette instruction génère un nombre entier aléatoire entre 0 et la longueur de la liste « data ». Le caractère correspondant à cet index est choisi puis ajouté à une variable « password ».
+    passwordOutput.value = password; 
 }
 generateButton.addEventListener("click", generateurPassword);
 
